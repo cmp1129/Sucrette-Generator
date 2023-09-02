7a8d05f1c5db267c872b76b3de0d715ed70da781
Cristina <cristinamateospaez@gmail.com>
2023-09-02 11:17:46 +0200

    Update random.js

--- js/random.js
+++ js/random.js
@@ -2,15 +2,16 @@ function getRandomIndex(max) {
     return Math.floor(Math.random() * max);
 }
 
+// Función que genera de forma Aleatoria un Avatar
 function randomizeAvatarAndClothes() {
     const centeredImagesRand = document.querySelectorAll('.centered-image');
     const overlayImagesRand = document.querySelectorAll('.overlay-image');
 
-    // Obtén un índice aleatorio para el avatar y la ropa
+    // 01. Obtenemos un índice aleatorio del avatar y la ropa
     const randomAvatarIndex = getRandomIndex(centeredImagesRand.length);
     const randomClothesIndex = getRandomIndex(overlayImagesRand.length);
 
-    // Muestra el avatar y la ropa aleatorios
+    // 02. Mostramos por pantalla el avatar y la ropa aleatoriamente
     centeredImagesRand.forEach((image, index) => {
         if (index === randomAvatarIndex) {
             image.style.display = 'block';
@@ -28,10 +29,8 @@ function randomizeAvatarAndClothes() {
     });
 }
 
-// Obtén una referencia al botón
 const randomizeButton = document.getElementById('randomize');
 
-// Agrega un EventListener al botón
 randomizeButton.addEventListener('click', () => {
     randomizeAvatarAndClothes(); 
 });
