5714c0db06f1708d59057db944bb99c8ac8b4c0f
Cristina <cristinamateospaez@gmail.com>
2023-09-02 16:09:06 +0200

    commit

--- js/message-button-crush.js
+++ js/message-button-crush.js
@@ -14,3 +14,34 @@ document.addEventListener("DOMContentLoaded", function () {
         messageContainerCrush.style.display = "none";
     });
 });
+
+// 00. Mini Crushes
+document.getElementById('castielButton').addEventListener('click', function() {
+    const imageSrc = this.getAttribute('data-image-src');
+    openImageInNewTab(imageSrc);
+});
+
+document.getElementById('nathButton').addEventListener('click', function() {
+    const imageSrc = this.getAttribute('data-image-src');
+    openImageInNewTab(imageSrc);
+});
+
+document.getElementById('kenButton').addEventListener('click', function() {
+    const imageSrc = this.getAttribute('data-image-src');
+    openImageInNewTab(imageSrc);
+});
+
+document.getElementById('lysButton').addEventListener('click', function() {
+    const imageSrc = this.getAttribute('data-image-src');
+    openImageInNewTab(imageSrc);
+});
+
+document.getElementById('arminButton').addEventListener('click', function() {
+    const imageSrc = this.getAttribute('data-image-src');
+    openImageInNewTab(imageSrc);
+});
+
+// Función para abrir la imagen en una nueva pestaña
+function openImageInNewTab(imageSrc) {
+    window.open(imageSrc, '_blank');
+}
