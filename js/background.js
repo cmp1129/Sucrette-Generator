7ef423aa71e53793fd7245507fdeee08a95e5458
Cristina <cristinamateospaez@gmail.com>
2023-09-02 09:35:58 +0200

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,12 +1,17 @@
-// BACKGROUND IMAGES
 const backgroundImages = document.querySelectorAll('.background-image');
 const changeBackgroundButton = document.getElementById('changeBackgroundButton');
 
 let currentBackgroundIndex = 0;
 
+// Función para cambiar la imagen de fondo
 function changeBackground() {
+    // 01. Ocultamos la imagen de fondo actual con opacidad 0
     backgroundImages[currentBackgroundIndex].style.opacity = 0;
+
+    // 02. Actualizamos el indice de la imagen del fondo
     currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
+
+    // 03. Mostramos la imagen con opacidad 1
     backgroundImages[currentBackgroundIndex].style.opacity = 1;
 }
 
